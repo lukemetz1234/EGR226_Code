b6536bdcd7273e8e2d3d51b30e209d08257f228d
koelzerb <koelzerb@KEN228-10.hpc.gvsu.edu>
2019-10-09 09:39:34 -0400

    first

--- EGR 226 Lab 7/main.c
+++ EGR 226 Lab 7/main.c
@@ -1,7 +1,10 @@
 #include "msp.h"
 
 void LCD_init();
-
+void pushNibble(uint8_t nibble);
+void pushByte(uint8_t byte);
+void commandWrite(uint8_t command);
+void delay_ms(int value);
 
 
 void main(void)
@@ -9,4 +12,12 @@ void main(void)
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
 	//test test
+}
+
+void delay_ms(int value)
+{
+    SysTick->LOAD = value*3000;  //3000 cycles in a millisecond
+    SysTick->VAL = 91719;  //Set Val to anything to clear //STCVR
+    SysTick->CTRL |= BIT0;  //Enable   //Status and Control Register STCSR
+    while((SysTick->CTRL&BIT(16))==0);  //Wait for SysTick to be complete
 }
