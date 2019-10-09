5999df826457171e32e5c9b63c9081df912a7429
lukemetz <lukemetz@35.40.170.229>
2019-10-09 11:53:36 -0400

    WOrks

--- EGR 226 Lab 7/main.c
+++ EGR 226 Lab 7/main.c
@@ -17,7 +17,7 @@ void main(void)
 	LCD_init();
 
 	while(1) {
-
+	    //commandWrite(0xF);
 	}
 }
 
@@ -26,44 +26,51 @@ void pin_init() {
     P6->SEL1 &= ~BIT1;
     P6->DIR |= BIT1;   //Output
 
-    P6->SEL0 &= ~BIT0;  //Set up pins P6.1 (E)
+    P6->SEL0 &= ~BIT0;  //Set up
     P6->SEL1 &= ~BIT0;
     P6->DIR |= BIT0;   //Output
 
     P4->SEL0 &= ~(BIT0 | BIT1 | BIT2 | BIT3);  //Set up pins
     P4->SEL1 &= ~(BIT0 | BIT1 | BIT2 | BIT3);
     P4->DIR |= (BIT0 | BIT1 | BIT2 | BIT3);   //Output
+    P4->OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
 
 }
 
 void LCD_init() {
      commandWrite(0x30);
-     delay_ms(100);
+     delay_ms(1000);
      commandWrite(0x30);
-     delay_micro(200);
+     delay_micro(2000);
      commandWrite(0x30);
-     delay_ms(100);
+     delay_ms(1000);
 
      commandWrite(2);
-     delay_micro(100);
+     delay_micro(1000);
      commandWrite(2);
-     delay_micro(100);
+     delay_micro(1000);
 
      commandWrite(8);
-     delay_micro(100);
+     delay_micro(1000);
      commandWrite(0x0C);
-     delay_micro(100);
+     delay_micro(1000);
      commandWrite(1);
-     delay_micro(100);
+     delay_micro(1000);
      commandWrite(6);
-     delay_ms(10);
-     commandWrite(1);
+     delay_ms(1000);
+
+     commandWrite(0xF);
+     delay_ms(100);
+     commandWrite(0xF);
+     delay_ms(100);
+     //commandWrite(0xF);
+     //delay_ms(100);
 }
 
 
 void pushNibble(uint8_t nibble) {
     P4->OUT &=~0x0F;
-    P5->OUT |= (nibble & 0x0F);
+    P4->OUT |= (nibble & 0x0F);
 }
 
 
