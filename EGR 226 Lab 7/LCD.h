1b1c76b0cc91f2b6111be32f3a0b25e8cf19f5b9
lukemetz <lukemetz@lukes-mbp-6.attlocal.net>
2019-10-13 12:05:48 -0400

    All comments added. Finished and working 100%

--- EGR 226 Lab 7/LCD.h
+++ EGR 226 Lab 7/LCD.h
@@ -1,11 +1,17 @@
-
+/*******************************************************
+* Name:              Luke Metz, Brian Koelzer
+* Course:            EGR 226 - Microcontroller Programming and Applications
+* Project:           Lab
+* File:              LCD.h
+* Description:       This file holds the headers for the LCD.c file
+*******************************************************/
 
 void LCDpin_init();
 void LCD_init();
 void pushNibble(uint8_t nibble);
 void pushByte(uint8_t byte);
 void commandWrite(uint8_t command);
-void dataWrite(uint8_t command);
+void dataWrite(uint8_t data);
 void delay_ms(int value);
 void pulseEnablePin();
 void delay_micro(int value);
--- EGR 226 Lab 7/main.c
+++ EGR 226 Lab 7/main.c
@@ -1,53 +1,56 @@
+/*******************************************************
+* Name:              Luke Metz, Brian Koelzer
+* Course:            EGR 226 - Microcontroller Programming and Applications
+* Project:           Lab 07 Interfacing the MSP432 with a Liquid Crystal Display (LCD)
+* File:              main.c
+* Description:       This program initializes the proper pins for an LCD display and runs a sequence to set up the display.
+*                    Each group members name is printed to the display, the first name on the top line and the second name on the
+*                    line below. EGR is printed in the line below that, followed by 226 on the very bottom line. Each segment
+*                    printed to the display is centered.
+*******************************************************/
+
 #include "msp.h"
 #include "LCD.h"
-
-
-/******************************
-Added in Lecture (Not finished)
-char display[65];           //what to put on the display
-char currentDisplay[65];    //Currently on it
-******************************/
-
 
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	LCDpin_init();
-	LCD_init();
+	LCDpin_init();  //Set up pins connected to the LCD display
+	LCD_init();     //Sequence to properly initialize LCD
 
-	char name1[4] = {'L', 'U', 'K', 'E'};        //Put whatever you want to print in a character array with the correct length
-	char name2[5] = {'B', 'R', 'I', 'A', 'N'};
-	char class[3] = {'E', 'G', 'R'};
-	char classNum[3] = {'2', '2', '6'};
+	char name1[4] = {'L', 'U', 'K', 'E'};       //Partner 1 name
+	char name2[5] = {'B', 'R', 'I', 'A', 'N'};  //Partner 2 name
+	char class[3] = {'E', 'G', 'R'};            //Course
+	char classNum[3] = {'2', '2', '6'};         //Course number
 
 	int k = 0;
 
-	commandWrite(0x86);                 //address  06
+	commandWrite(0x86);         //Address  86
 	for(k = 0; k < 4; k++) {
-	    dataWrite(name1[k]);             //Write each letter
+	    dataWrite(name1[k]);    //Write each letter of LUKE
 	    delay_ms(10);
 	}
 
-	commandWrite(0xC5);
+	commandWrite(0xC5);         //Address  C5
 	for(k = 0; k < 5; k++) {
-	    dataWrite(name2[k]);             //Write each letter
+	    dataWrite(name2[k]);    //Write each letter of BRIAN
 	    delay_ms(10);
 	}
 
-	commandWrite(0x96);
+	commandWrite(0x96);         //Address 96
     for(k = 0; k < 3; k++) {
-        dataWrite(class[k]);             //Write each letter
+        dataWrite(class[k]);     //Write each letter of EGR
         delay_ms(10);
     }
 
-    commandWrite(0xD6);
+    commandWrite(0xD6);         //Address D6
     for(k = 0; k < 3; k++) {
-        dataWrite(classNum[k]);             //Write each letter
+        dataWrite(classNum[k]);  //Write each number of 226
         delay_ms(10);
     }
 
-    commandWrite(0x0C);
+    commandWrite(0x0C);     //Turn off cursor
 }
 
 
