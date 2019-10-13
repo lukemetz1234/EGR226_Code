4ab53f028a9a2e46e3ac5eb96b0a1bb0f1b2cfa1
lukemetz <lukemetz@lukes-mbp-6.attlocal.net>
2019-10-12 21:47:02 -0400

    Prints properly to the LCD screen. Changed lcd init function to what was in power point and moved pulseEnablePin function.

--- EGR 226 Lab 7/main.c
+++ EGR 226 Lab 7/main.c
@@ -5,12 +5,14 @@ void LCD_init();
 void pushNibble(uint8_t nibble);
 void pushByte(uint8_t byte);
 void commandWrite(uint8_t command);
+void dataWrite(uint8_t command);
 void delay_ms(int value);
 void pulseEnablePin();
 void delay_micro(int value);
 
+
 /******************************
-Added in Lecture (probably not finished)
+Added in Lecture (Not finished)
 char display[65];           //what to put on the display
 char currentDisplay[65];    //Currently on it
 ******************************/
@@ -23,8 +25,17 @@ void main(void)
 	pin_init();
 	LCD_init();
 
+	char name[8] = {'L', 'U', 'K', 'E'};        //Put whatever you want to print in a character array with the correct length
+	int j = 1;
+	int k = 0;
 	while(1) {
-	    //commandWrite(0xF);
+	    if (j == 1) {                           //This is so that it dose not repeat forever
+	        for(k = 0; k < 4; k++) {
+	            dataWrite(name[k]);     //Write each letter
+	            delay_ms(500);
+	        }
+	    }
+	    j++;
 	}
 }
 
@@ -58,55 +69,52 @@ void pin_init() {
 
 }
 
-void LCD_init() {
-     commandWrite(0x30);
-     delay_ms(1000);
-     commandWrite(0x30);
-     delay_micro(2000);
-     commandWrite(0x30);
-     delay_ms(1000);
 
-     commandWrite(2);
-     delay_micro(1000);
-     commandWrite(2);
-     delay_micro(1000);
+void LCD_init(void)
+{
+  commandWrite(0x03);//REST 3 times
+  delay_ms(10);
+  commandWrite(0x03);
+  delay_ms(10);
+  commandWrite(0x03);
+  delay_ms(10);
 
-     commandWrite(8);
-     delay_micro(1000);
-     commandWrite(0x0C);
-     delay_micro(1000);
-     commandWrite(1);
-     delay_micro(1000);
-     commandWrite(6);
-     delay_ms(1000);
-
-     commandWrite(0xF);
-     delay_ms(100);
-     commandWrite(0xF);
-     delay_ms(100);
-     //commandWrite(0xF);
-     //delay_ms(100);
+  commandWrite(0x02);//4bit mode
+  delay_micro(100);
+  commandWrite(0x02);
+  commandWrite(0x06);//2 lin 5x7 format
+  delay_micro(100);
+  commandWrite(0x0F);//display on cursor blinking
+  delay_micro(100);
+  commandWrite(0x01);//clears display
+  delay_micro(100);
+  commandWrite(0x06);//incremnt cursor
+  delay_micro(100);
 }
 
 
 void pushNibble(uint8_t nibble) {
     P4->OUT &=~0x0F;
     P4->OUT |= (nibble & 0x0F);
+    pulseEnablePin();
 }
 
 
 void pushByte(uint8_t byte) {
-    pushNibble(byte >> 4);
-    pulseEnablePin();
+    pushNibble(byte>>4);
     pushNibble(byte & 0x0F);
-    pulseEnablePin();
+    delay_micro(100);
 }
 
 
 void commandWrite(uint8_t command) {
     P6->OUT &= ~BIT0;
     pushByte(command);
-    pulseEnablePin();
+}
+
+void dataWrite(uint8_t command) {
+    P6->OUT |= BIT0;
+    pushByte(command);
 }
 
 void pulseEnablePin() {
